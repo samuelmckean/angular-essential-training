d50ac1f74ae91eb6f0c06f679b04c724fc791e18
Samuel McKean <samuel.mckean@gmail.com>
2020-05-07 17:02:55 -0500

    Added form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MediaItemComponent } from './media-item/media-item.component';
 import { MediaItemListComponent } from './media-item-list/media-item-list.component';
 import { FavoriteDirective } from './favorite.directive'
 import { CategoryListPipe } from './category-list.pipe';
+import { MediaItemFormComponent } from './media-item-form/media-item-form.component';
 
 @NgModule({
     imports: [
-        BrowserModule
+        BrowserModule,
+        ReactiveFormsModule
     ],
     declarations: [
         AppComponent,
         MediaItemComponent,
         MediaItemListComponent,
         FavoriteDirective,
-        CategoryListPipe
+        CategoryListPipe,
+        MediaItemFormComponent
     ],
     bootstrap: [
         AppComponent
