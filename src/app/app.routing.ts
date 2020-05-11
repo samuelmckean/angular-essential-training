db0fa73abe2c8928ac9cdf67555c0cb157d88581
Samuel McKean <samuel.mckean@gmail.com>
2020-05-11 13:14:51 -0500

    Added routing

---
+++ src/app/app.routing.ts
@@ -0,0 +1,12 @@
+import { Routes, RouterModule } from '@angular/router';
+import { MediaItemListComponent } from './media-item-list/media-item-list.component';
+
+const appRoutes: Routes = [
+    { path: 'add',
+      loadChildren: () => import('./media-item/new-item.module').then(m => m.NewItemModule)
+    },
+    { path: ':medium', component: MediaItemListComponent },
+    { path: '', redirectTo: 'all', pathMatch: 'full' }
+];
+
+export const routing = RouterModule.forRoot(appRoutes);
